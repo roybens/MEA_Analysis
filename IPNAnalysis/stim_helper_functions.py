586c4d3b7423b737cc7854be142cc30d5e083916
Nate Maffly <ncmaffly@ucdavis.edu>
2025-02-04 00:36:19 -0800

    new file for helper functions

--- IPNAnalysis/stim_helper_functions.py
+++ IPNAnalysis/stim_helper_functions.py
@@ -1,8 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
+from scipy.signal import butter, filtfilt
+
+def plot_frequency_spectrum(trace, fs):
+    """
+    Plots the frequency spectrum of a given trace.
+    trace: The signal (array of amplitudes).
+    fs: Sampling frequency (in Hz).
+    """
+    # Calculate the FFT of the signal
+    fft_result = np.fft.fft(trace)
+    
+    # Compute the corresponding frequencies
+    freqs = np.fft.fftfreq(len(trace), d=1/fs)
+    
+    # Take the magnitude of the FFT (absolute value)
+    fft_magnitude = np.abs(fft_result)
+    
+    # Only consider positive frequencies
+    positive_freqs = freqs[freqs >= 0]
+    positive_magnitude = fft_magnitude[freqs >= 0]
+    
+    # Plot the frequency spectrum
+    plt.figure(figsize=(10, 6))
+    plt.plot(positive_freqs, positive_magnitude)
+    plt.title("Frequency Spectrum of the Signal")
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Amplitude")
+    plt.xlim([0, fs/2])  # Nyquist limit
+    plt.grid()
+    plt.show()
 
 
+def bandpass_filter(trace, fs, lowcut, highcut, order=3):
+    nyquist = fs / 2
+    low = lowcut / nyquist
+    high = highcut / nyquist
+    b, a = butter(order, [low, high], btype='band')
+    filtered_trace = filtfilt(b, a, trace)
+    return filtered_trace
 
 def polynomial_interpolation(trace, start_idx, end_idx, degree=3):
     # Define x and y points outside the artifact region
