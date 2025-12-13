61e4cefafb91edc728bba4f1db9862d954ec4cb8
mandarmp <mandarmpatil@outlook.com>
2025-12-12 16:16:48 -0800

    Docker build configs for spikesorting

--- dockers/spikesorter/entrypoint.sh
+++ dockers/spikesorter/entrypoint.sh
@@ -10,6 +10,14 @@ cd "$REPO_DIR"
 git fetch origin "$BRANCH" --depth=1 || true
 git reset --hard origin/"$BRANCH" || true
 
+echo "=== RUNNING PRE-PIPELINE HOOKS ==="
+#excute docker patches before running the pipeline
+if [ -f "$REPO_DIR/dockers/spikesorter/docker_patches.sh" ]; then
+    echo "Executing docker patches..."
+    chmod +x "$REPO_DIR/dockers/spikesorter/docker_patches.sh"
+    bash "$REPO_DIR/dockers/spikesorter/docker_patches.sh"
+fi
+
 
 echo "=== Running pipeline ==="
 exec python3 "$REPO_DIR/IPNAnalysis/run_pipeline_driver.py" "$@"
