0efb0a45b2a92eb2c092b236c785420b4f097a2a
adamm <amwe@ucdavis.edu>
2026-01-27 11:17:19 -0800

    added setup files to import IPNAnalysis into axon_reconstructor. Added install artifacts to .gitignore

---
+++ setup.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from setuptools import setup
+
+
+# This repository is both the VCS root and the top-level Python package directory.
+# We provide explicit package mappings so `pip install -e .` supports imports like:
+#   import MEA_Analysis
+#   from MEA_Analysis.IPNAnalysis import mea_analysis_routine
+setup(
+    name="MEA_Analysis",
+    version="0.0.0",
+    packages=[
+        "MEA_Analysis",
+        "MEA_Analysis.IPNAnalysis",
+        "MEA_Analysis.MEAProcessingLibrary",
+    ],
+    package_dir={
+        "MEA_Analysis": ".",
+        "MEA_Analysis.IPNAnalysis": "IPNAnalysis",
+        "MEA_Analysis.MEAProcessingLibrary": "MEAProcessingLibrary",
+    },
+)
